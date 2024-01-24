f887a0da6f29b4ddd65f3f64ab1ceadc627ae2d0
Bairu <bairuli1234@gmail.com>
2024-01-24 11:08:39 -0800

    add collision to all entities

--- backgroundObjects.js
+++ backgroundObjects.js
@@ -1,17 +1,17 @@
 class BackgroundObject {
-    constructor(x, y, width, height, scale, mickey) {
-        Object.assign(this, { x, y, width, height, scale, mickey });
+    constructor(x, y, width, height, scale, game) {
+        Object.assign(this, { x, y, width, height, scale, game });
     }
 
-    handleCollision() {
-        let overlap = this.BB.overlapBB(this.mickey.BB);
-        let sig = { x: Math.sign(this.BB.x - this.mickey.BB.x), y: Math.sign(this.BB.y - this.mickey.BB.y) };
+    handleCollision(entity) {
+        let overlap = this.BB.overlapBB(entity.BB);
+        let sig = { x: Math.sign(this.BB.x - entity.BB.x), y: Math.sign(this.BB.y - entity.BB.y) };
         // console.log("[X: sig " + sig.x + ", dif " + overlap.x + "], [Y: sig " + sig.y + ", dif " + overlap.y + "]");
 
         if (overlap.x < overlap.y) {
-            this.mickey.x -= (overlap.x + 1) * sig.x;
+            entity.x -= (overlap.x + 1) * sig.x;
         } else {
-            this.mickey.y -= (overlap.y + 1) * sig.y;
+            entity.y -= (overlap.y + 1) * sig.y;
         }
     }
 
@@ -31,8 +31,8 @@ class BackgroundObject {
 }
 
 class DestroyedDesertTower extends BackgroundObject {
-    constructor(x, y, width, height, scale, mickey) {
-        super(x, y, width, height, scale, mickey);
+    constructor(x, y, width, height, scale, game) {
+        super(x, y, width, height, scale, game);
         this.updateSpritesheet("./assets/background/destroyedDesertTower.png");
         // change BB
         this.offsetBB = { x: 44, y: 10, w: -80, h: -20 };
@@ -40,98 +40,112 @@ class DestroyedDesertTower extends BackgroundObject {
     }
 
     update() {
-        if (this.BB.collideBB(this.mickey.BB)) {
-            this.handleCollision();
-        }
+        this.game.entities.forEach(entity => {
+            if (this.BB.collideBB(entity.BB)) {
+                this.handleCollision(entity);
+            }
+        });
     }
 }
 
 class BarbedWire extends BackgroundObject {
-    constructor(x, y, width, height, scale, mickey) {
-        super(x, y, width, height, scale, mickey);
+    constructor(x, y, width, height, scale, game) {
+        super(x, y, width, height, scale, game);
         this.updateSpritesheet("./assets/background/barbedwire1.png");
         this.offsetBB = { x: 0, y: 0, w: 0, h: 0 };
         this.updateBB(this.offsetBB);
     }
 
     update() {
-        if (this.BB.collideBB(this.mickey.BB)) {
-            this.handleCollision();
-        }
+        this.game.entities.forEach(entity => {
+            if (this.BB.collideBB(entity.BB)) {
+                this.handleCollision(entity);
+            }
+        });
     }
 }
 
 class DesertTower extends BackgroundObject {
-    constructor(x, y, width, height, scale, mickey) {
-        super(x, y, width, height, scale, mickey);
+    constructor(x, y, width, height, scale, game) {
+        super(x, y, width, height, scale, game);
         this.updateSpritesheet("./assets/background/deserttower.png");
         this.offsetBB = { x: 34, y: 0, w: -61, h: 0 };
         this.updateBB(this.offsetBB);
     }
 
     update() {
-        if (this.BB.collideBB(this.mickey.BB)) {
-            this.handleCollision();
-        }
+        this.game.entities.forEach(entity => {
+            if (this.BB.collideBB(entity.BB)) {
+                this.handleCollision(entity);
+            }
+        });
     }
 }
 
 class DeadTree extends BackgroundObject {
-    constructor(x, y, width, height, scale, mickey) {
-        super(x, y, width, height, scale, mickey);
+    constructor(x, y, width, height, scale, game) {
+        super(x, y, width, height, scale, game);
         this.updateSpritesheet("./assets/background/deadtree.png");
         this.offsetBB = { x: 32, y: 8, w: -64, h: -10 };
         this.updateBB(this.offsetBB);
     }
 
     update() {
-        if (this.BB.collideBB(this.mickey.BB)) {
-            this.handleCollision();
-        }
+        this.game.entities.forEach(entity => {
+            if (this.BB.collideBB(entity.BB)) {
+                this.handleCollision(entity);
+            }
+        });
     }
 }
 
 class DeadBody extends BackgroundObject {
-    constructor(x, y, width, height, scale, mickey) {
-        super(x, y, width, height, scale, mickey);
+    constructor(x, y, width, height, scale, game) {
+        super(x, y, width, height, scale, game);
         this.updateSpritesheet("./assets/background/deadbodies1.png");
         this.offsetBB = { x: 0, y: 4, w: 0, h: -6 };
         this.updateBB(this.offsetBB);
     }
 
     update() {
-        if (this.BB.collideBB(this.mickey.BB)) {
-            this.handleCollision();
-        }
+        this.game.entities.forEach(entity => {
+            if (this.BB.collideBB(entity.BB)) {
+                this.handleCollision(entity);
+            }
+        });
     }
 }
 
 class EmptyBarrel extends BackgroundObject {
-    constructor(x, y, width, height, scale, mickey) {
-        super(x, y, width, height, scale, mickey);
+    constructor(x, y, width, height, scale, game) {
+        super(x, y, width, height, scale, game);
         this.updateSpritesheet("./assets/background/emptybarrel1.png");
         this.offsetBB = { x: 22, y: 24, w: -46, h: -28 };
         this.updateBB(this.offsetBB);
     }
 
     update() {
-        if (this.BB.collideBB(this.mickey.BB)) {
-            this.handleCollision();
-        }
+        this.game.entities.forEach(entity => {
+            if (this.BB.collideBB(entity.BB)) {
+                this.handleCollision(entity);
+            }
+        });
     }
 }
 
 class WallmartStoneHenge extends BackgroundObject {
-    constructor(x, y, width, height, scale, mickey) {
-        super(x, y, width, height, scale, mickey);
+    constructor(x, y, width, height, scale, game) {
+        super(x, y, width, height, scale, game);
         this.updateSpritesheet("./assets/background/walmartStoneHenge.png");
         this.offsetBB = { x: 37, y: 9, w: -68, h: -30 };
         this.updateBB(this.offsetBB);
     }
 
     update() {
-        if (this.BB.collideBB(this.mickey.BB)) {
-            this.handleCollision();
-        }
+        this.game.entities.forEach(entity => {
+            if (this.BB.collideBB(entity.BB)) {
+                this.handleCollision(entity);
+            }
+        });
     }
 }
