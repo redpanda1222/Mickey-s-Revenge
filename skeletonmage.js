a4b1fe2b3028ead02b001c4dd8c1c21d11974713
redpanda1222 <soelin12usa@gmail.com>
2024-01-24 18:00:45 -0800

    edited skeletonmage

--- skeletonmage.js
+++ skeletonmage.js
@@ -1,64 +1,53 @@
-class skeletonMage {
+class SkeletonMage { // 
     constructor(game, mickey, x, y) {
         this.game = game;
         this.mickey = mickey;
 
         this.x = x;
         this.y = y;
-        this.w = 32;
-        this.h = 47;
-        this.speed = 21;
+        this.w = 50;
+        this.h = 50;
+        this.speed = 25;
 
-        // this.elapsedTime = 0;
-        // this.frameCount = 8;
-        // this.frameDuration = 0.1;
-        // this.x = 0;
-        // this.y = 0
-
-        this.size = 0;
-        this.facing = 0;
-        this.state = 0;
-        this.velocity = 0;
-        this.animation = [];
-        this.loadAnimations();
-
+        this.elapsedTime = 0;
+        this.frameCount = 2;
+        this.frameDuration = 0.3;
+        this.spritesheet = ASSET_MANAGER.getAsset("./assets/enemy/skeletonmage.png");
         this.totalTime = this.frameCount * this.frameDuration;
      
-        // this.xStart = 0;
-        // this.yStart = 204;
-        this.width = 100;
-        this.height = 100;
-        
-        // this.flip = 0;
+        this.xStart = 1;
+        this.yStart = 48;
+        this.width = 32;
+        this.height = 46;
 
         //Rectangle bounding box
-        this.offsetBB = {x: 15, y: 2, w: -30, h: -15};
-        this.BB = new BoundingBox(this.x + this.offsetBB.x, this.y + this.offsetBB.y, this.w + this.offsetBB.w, this.h + this.offsetBB.h);
+        this.offsetBB = {x: -3, y: 0, w: -20, h: 0};
+        this.BB = new BoundingBox(this.x + this.offsetBB.x, this.y, this.w + this.offsetBB.y, this.h);
     };
 
     update() {
         if (this.mickey.x < this.x) {
             this.x -= this.speed * this.game.clockTick;
-            this.flip = 1; // Flip the sprite if moving left
+            //this.flip = 1; // Flip the sprite if moving left
             this.xStart = 1;
-            this.yStart = 1;
+            this.yStart = 48;
         } 
         if (this.mickey.x > this.x) {
             this.x += this.speed * this.game.clockTick;
-            this.flip = 2; // Do not flip the sprite if moving right
+            //this.flip = 0; // Do not flip the sprite if moving right
             this.xStart = 1;
             this.yStart = 96;
         } 
-        // if (this.mickey.x == this.x) {
-        //     this.x += this.speed * this.game.clockTick;
-        //     this.flip = 1;
-        // }
-        // if (this.mickey.y < this.y) {
-        //     this.y -= this.speed * this.game.clockTick;
-        // } 
-        // if (this.mickey.y > this.y) {
-        //     this.y += this.speed * this.game.clockTick;
-        // }
+        if (this.mickey.x == this.x) {
+            this.x += this.speed * this.game.clockTick;
+            //this.flip = 1;
+        }
+        if (this.mickey.y < this.y) {
+            this.y -= this.speed * this.game.clockTick;
+        } 
+        if (this.mickey.y > this.y) {
+            this.y += this.speed * this.game.clockTick;
+        }
 
         // update bounding box
         this.BB.x = this.x + this.offsetBB.x;
@@ -68,62 +57,18 @@ class skeletonMage {
             console.log("Skeleton mage!!!");
         }
     };
-    loadAnimations() {
-        // facing forward
-        this.animation.push(new Animator(ASSET_MANAGER.getAsset("./assets/enemy/skeletonmage.png"), 1, 1, 47, 32, 3, 0.3));
-        // facing left
-        this.animation.push(new Animator(ASSET_MANAGER.getAsset("./assets/enemy/skeletonmage.png"), 1, 48, 47, 32, 3, 0.3));
-        // facing right
-        this.animation.push(new Animator(ASSET_MANAGER.getAsset("./assets/enemy/skeletonmage.png"), 1, 96, 47, 32, 3, 0.3));
-        // the back
-        this.animation.push(new Animator(ASSET_MANAGER.getAsset("./assets/enemy/skeletonmage.png"), 1, 145, 47, 32, 3, 0.3));
-    };
+
     draw(ctx) {
         this.elapsedTime += this.game.clockTick;
         const frame = this.currentFrame();
         if (this.elapsedTime > this.totalTime) this.elapsedTime -= this.totalTime;
-   
-        if (this.flip == 0) {
-            // ctx.drawImage(this.spritesheet,
-            //     this.xStart + this.width*frame, this.yStart,
-            //     this.width, this.height,
-            //     this.x, this.y,
-            //     this.w, this.h);
-                this.animation[0].drawFrame(this.game.clockTick, ctx, this.x, this.y, this.width, this.height);
-        }
-        if (this.flip == 1) {
-            // ctx.drawImage(this.spritesheet,
-            //     this.xStart - this.width*frame, this.yStart,
-            //     this.width, this.height,
-            //     this.x, this.y,
-            //     this.w, this.h);
-                this.animation[1].drawFrame(this.game.clockTick, ctx, this.x, this.y, this.width, this.height);
-        }
-        if (this.flip == 2) {
-            // ctx.drawImage(this.spritesheet,
-                // this.xStart + this.width*frame, this.yStart,
-                // this.width, this.height,
-                // this.x, this.y,
-                // this.w, this.h);
-                this.animation[2].drawFrame(this.game.clockTick, ctx, this.x, this.y, this.width, this.height);
-        }
-        if (this.flip == 3) {
-            // ctx.drawImage(this.spritesheet,
-            //     this.xStart - this.width*frame, this.yStart,
-            //     this.width, this.height,
-            //     this.x, this.y,
-            //     this.w, this.h);
-                this.animation[3].drawFrame(this.game.clockTick, ctx, this.x, this.y, this.width, this.height);
-        }
-        // if (this.facing == 0) {
-        //     this.animation[0].drawFrame(this.game.clockTick, ctx, this.x, this.y, this.width, this.height);
-        // } else if (this.facing == 1) {
-        //     this.animation[2].drawFrame(this.game.clockTick, ctx, this.x, this.y, this.width, this.height);
-        // } else if (this.facing == 2) {
-        //     this.animation[1].drawFrame(this.game.clockTick, ctx, this.x, this.y, this.width, this.height);
-        // } else if (this.facing == 3) {
-        //     this.animation[3].drawFrame(this.game.clockTick, ctx, this.x, this.y, this.width, this.height);
-        // };
+
+        ctx.drawImage(this.spritesheet,
+            this.xStart + this.width*frame, this.yStart,
+            this.width, this.height,
+            this.x, this.y,
+            this.w, this.h);
+
         if (PARAMS.DEBUG) {
             // draws bounding box
             this.BB.draw(ctx);
