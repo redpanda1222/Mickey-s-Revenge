c32b2ee6f31ab828c152fb7d562fbc9db92e4a73
Bairu <bairuli1234@gmail.com>
2024-02-12 11:59:01 -0800

    made debug screen

--- src/background/levels.js
+++ src/background/levels.js
@@ -48,79 +48,79 @@ var levelOne = {
     waves: [
         {
             time: 0,       // spawn listed enemies after this time (seconds)
-            spawnrate: 60, // lower for faster spawnrate (60 = 1 second)
+            spawnrate: 10, // lower for faster spawnrate (60 = 1 second)
             skeleton: 1    // enemyType and its weight (higher weight means higher chance to spawn)
         },
-        {
-            time: 20,
-            spawnrate: 40,
-            skeleton: 2, // skeleton has 2 / 4 chance to spawn
-            bird: 1,     // bird has 1 / 4 chance to spawn
-            huskydog: 1  // huskydog has 1 / 4 chance to spawn
-        },
-        {
-            time: 40,
-            spawnrate: 30, // (30 = 0.5 second, 15 = 0.25 second, etc.)
-            skeleton: 5,   // skeleton has 5 / 11 chance to spawn
-            bird: 2,       // bird has 2 / 11 chance to spawn
-            huskydog: 3,
-            skeletonmage: 1
-        },
-        {
-            time: 70,
-            spawnrate: 60,
-            skeletonmage: 1
-        }
+        // {
+        //     time: 20,
+        //     spawnrate: 40,
+        //     skeleton: 2, // skeleton has 2 / 4 chance to spawn
+        //     bird: 1,     // bird has 1 / 4 chance to spawn
+        //     huskydog: 1  // huskydog has 1 / 4 chance to spawn
+        // },
+        // {
+        //     time: 40,
+        //     spawnrate: 30, // (30 = 0.5 second, 15 = 0.25 second, etc.)
+        //     skeleton: 5,   // skeleton has 5 / 11 chance to spawn
+        //     bird: 2,       // bird has 2 / 11 chance to spawn
+        //     huskydog: 3,
+        //     skeletonmage: 1
+        // },
+        // {
+        //     time: 70,
+        //     spawnrate: 60,
+        //     skeletonmage: 1
+        // }
     ],
 
-    formations: [
-        {
-            spawntime: [10, 20], // at what time it spawns (in seconds)
-            moveVector: { x: 1, y: 0 }, // the direction that each enemy below will take (null = towards mickey)
-            despawnTime: 10, // all despawns after this time (null = no despawn)
+    // formations: [
+    //     {
+    //         spawntime: [10, 20], // at what time it spawns (in seconds)
+    //         moveVector: { x: 1, y: 0 }, // the direction that each enemy below will take (null = towards mickey)
+    //         despawnTime: 10, // all despawns after this time (null = no despawn)
 
-            // skeleton wall
-            skeleton: [
-                // x and y is relative to the camera
-                { x: 10, y: 10 },
-                { x: 10, y: 20 },
-                { x: 10, y: 30 },
-                { x: 10, y: 40 },
-                { x: 10, y: 50 },
-            ]
-        },
-        {
-            spawntime: [30],
-            moveVector: { x: -1, y: -1 },
-            despawnTime: 20,
+    //         // skeleton wall
+    //         skeleton: [
+    //             // x and y is relative to the camera
+    //             { x: 10, y: 10 },
+    //             { x: 10, y: 20 },
+    //             { x: 10, y: 30 },
+    //             { x: 10, y: 40 },
+    //             { x: 10, y: 50 },
+    //         ]
+    //     },
+    //     {
+    //         spawntime: [30],
+    //         moveVector: { x: -1, y: -1 },
+    //         despawnTime: 20,
 
-            // dog stampede
-            huskydog: [
-                { x: 1000, y: 1000 },
-                { x: 1000, y: 1010 },
-                { x: 1000, y: 1020 },
-                { x: 1010, y: 1000 },
-                { x: 1020, y: 1000 },
-                { x: 1010, y: 1010 },
-                { x: 1020, y: 1020 },
-                { x: 1010, y: 1020 },
-                { x: 1020, y: 1010 },
-            ]
-        },
-        {
-            spawntime: [40],
-            moveVector: { x: 0, y: 0 },
-            despawnTime: 10,
+    //         // dog stampede
+    //         huskydog: [
+    //             { x: 1000, y: 1000 },
+    //             { x: 1000, y: 1010 },
+    //             { x: 1000, y: 1020 },
+    //             { x: 1010, y: 1000 },
+    //             { x: 1020, y: 1000 },
+    //             { x: 1010, y: 1010 },
+    //             { x: 1020, y: 1020 },
+    //             { x: 1010, y: 1020 },
+    //             { x: 1020, y: 1010 },
+    //         ]
+    //     },
+    //     {
+    //         spawntime: [40],
+    //         moveVector: { x: 0, y: 0 },
+    //         despawnTime: 10,
 
-            // skeletonmage circle
-            skeletonmage: [
-                { x: PARAMS.WIDTH / 2, y: 0 },
-                { x: PARAMS.WIDTH / 2, y: PARAMS.HEIGHT },
-                { x: 0, y: PARAMS.HEIGHT / 2 },
-                { x: PARAMS.WIDTH, y: PARAMS.HEIGHT / 2 }
-            ]
-        }
-    ]
+    //         // skeletonmage circle
+    //         skeletonmage: [
+    //             { x: PARAMS.WIDTH / 2, y: 0 },
+    //             { x: PARAMS.WIDTH / 2, y: PARAMS.HEIGHT },
+    //             { x: 0, y: PARAMS.HEIGHT / 2 },
+    //             { x: PARAMS.WIDTH, y: PARAMS.HEIGHT / 2 }
+    //         ]
+    //     }
+    // ]
 
 }
 
--- src/player/mickey.js
+++ src/player/mickey.js
@@ -116,7 +116,7 @@ class Mickey {
         //add attack
         if (Math.floor(this.elapsedTime) < 2 && !this.attacking){
             //this.game.addAttackEntity(new FireSlash(this.game, this, 1 + (Math.floor(this.Level/100))));
-            this.game.addAttackEntity(new FireBreath(this.game, this, 1 + (Math.floor(this.Level/100)), 2));
+            // this.game.addAttackEntity(new FireBreath(this.game, this, 1 + (Math.floor(this.Level/100)), 2));
             this.attacking = true
         }
 
